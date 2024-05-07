ed16001ff1c47a9104cfc1913ec7504bd511b6ff
AndreArroyo9 <aarroyosa23dw@ikzubirimanteo.com>
2024-05-07 20:10:18 +0200

    Archivos de javadoc generados, directorio /target/site pusheado

---
+++ target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.ejemplo"},{"l":"com.ejemplo.clases"}];updateSearchResults();
---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.ejemplo.clases","l":"Alumno"},{"p":"com.ejemplo","l":"App"},{"p":"com.ejemplo","l":"FuncionesADocumentar"}];updateSearchResults();
